86efd20dc0cfd3f0e8447c4663babf6c3893523b
Liam Harbec <liam.harbec@gmail.com>
2020-05-06 12:48:27 -0700

    Fixed a bug relating to the naming scheme

--- app/controllers/item_controller.rb
+++ app/controllers/item_controller.rb
@@ -1,5 +1,7 @@
 #This controller will keep track of the items made, creating a type of lootbox.
 
 class ItemController < ApplicationController
-  @items = []
+  def index
+    @items = []
+  end
 end
