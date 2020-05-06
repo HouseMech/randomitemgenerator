ee4ab3936a1d3f9414454f6ea339fbc82bc12f2b
Liam Harbec <liam.harbec@gmail.com>
2020-05-06 12:19:07 -0700

    Added item and home page controllers

---
+++ app/controllers/item_controller_controller.rb
@@ -0,0 +1,5 @@
+#This controller will keep track of the items made, creating a type of lootbox.
+
+class ItemControllerController < ApplicationController
+  @items = []
+end
---
+++ test/controllers/item_controller_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class ItemControllerControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
