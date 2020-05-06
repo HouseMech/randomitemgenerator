5d93e7a83c11af52d61deda82a0ef6e64db35630
Liam Harbec <liam.harbec@gmail.com>
2020-05-06 13:18:47 -0700

    Added colours to Item model

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,12 +1,20 @@
 class Item
-  attr_reader :prefix, :weapon #Requires two attributes for an Item
+#The item class has three properties, a prefix, a weapon and a colour. Colour and Prefix are linked.
+#Prefixes, while being a neat addition to the item, also come with a colour coding indicating their rarity.
+  attr_reader :prefix, :weapon, :colour
 
 
-  PREFIXES = ["Hardy","Strong","Weak","Narrow"]
+  PREFIXES =
+  [{name: "Hardy", colour: "#CEAE32"},
+    {name: "Strong", colour: "#CEAE32"},
+    {name: "Weak", colour: "#03A9F4"},
+    {name: "Narrow", colour: "#02F5C8"}]
   WEAPON_NAMES = ["Sword","Axe","Bow","Gun"]
 
   def initialize
-    @prefix = PREFIXES.sample
+    @prefix_and_colour = PREFIXES.sample
+    @prefix = @prefix_and_colour[:name]
+    @colour = @prefix_and_colour[:colour]
     @weapon = WEAPON_NAMES.sample
   end
 
